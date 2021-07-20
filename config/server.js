3931b6c585e621ce992dc7a8615bcd3514203e08
Oliver <oliver@Olivers-MacBook-Pro.local>
2021-07-20 12:03:05 +0200

    Changing port

--- config/server.js
+++ config/server.js
@@ -1,6 +1,6 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 443),
+  port: env.int('PORT', 80),
   admin: {
     auth: {
       secret: env('ADMIN_JWT_SECRET', '41ab20526616457b1b85bc4aeec573b2'),
