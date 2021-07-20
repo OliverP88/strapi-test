cc49d270fe322aa21c5495479e4706773bdb7d16
Oliver <oliver@Olivers-MacBook-Pro.local>
2021-07-20 12:38:59 +0200

    Yarn commands

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -12,8 +12,8 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
-npm install
+yarn install
 
-npm run build
+yarn build
 #start our node app in the background
-npm run start > app.out.log 2> app.err.log < /dev/null &
+yarn start > app.out.log 2> app.err.log < /dev/null &
