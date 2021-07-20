c9afa3061f93150f69c1d895c07bc5526701cd4f
Oliver <oliver@Olivers-MacBook-Pro.local>
2021-07-20 11:11:21 +0200

    Added commands to start strapi

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -7,12 +7,13 @@ sudo chmod -R 777 /home/ec2-user/express-app
 cd /home/ec2-user/express-app
 
 #add npm and node to path
-export NVM_DIR="$HOME/.nvm"	
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm	
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install node modules
 npm install
 
+npm run build
 #start our node app in the background
-node app.js > app.out.log 2> app.err.log < /dev/null & 
+npm run start > app.out.log 2> app.err.log < /dev/null &
