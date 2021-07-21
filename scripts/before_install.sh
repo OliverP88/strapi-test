b9a3b283c6abb31e364f962c2d303fc2658cbf22
Oliver <oliver@Olivers-MacBook-Pro.local>
2021-07-21 08:44:01 +0200

    New commit

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-#download node and npm
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 12.14.1
-
-nvm use 12.14.1
+##download node and npm
+#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+#. ~/.nvm/nvm.sh
+#nvm install 12.14.1
+#
+#nvm use 12.14.1
 
 #create our working directory if it doesnt exist
 DIR="/home/ec2-user/strapi"
