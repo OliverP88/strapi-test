01b649775a92d7e22ac4d7a13c79ab8ab449a427
Oliver <oliver@Olivers-MacBook-Pro.local>
2021-07-21 09:13:30 +0200

    New commit

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
 
 #give permission for everything in the strapi directory
-#sudo chmod -R 777 /home/ec2-user/strapi
+sudo chmod -R 777 /home/ec2-user/strapi
 
 #navigate into our working directory where we have all our github files
-#cd /home/ec2-user/strapi
+cd /home/ec2-user/strapi
 
 #add npm and node to path
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
-#[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # loads nvm bash_completion (node is in path now)
 
 #install yarn global
-#npm install --global yarn
+npm install --global yarn
 
 #install node modules
 yarn install
 
 #yarn build
 #start our node app in the background
-yarn develop > app.out.log 2> app.err.log < /dev/null &
+yarn start > app.out.log 2> app.err.log < /dev/null &
